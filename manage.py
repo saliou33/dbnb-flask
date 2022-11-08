b0d4949efdc5010bd36ad8029700f03c0bb08e22
saliou33 <107521279+saliou33@users.noreply.github.com>
2022-11-08 20:59:54 +0000

    Add new files:

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from api.models.users import User
 from api.models.groupes import Groupe
 from api.models.qrcodes import Qrcode
 
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, migrate=migrate, User=User, Demandeur=Demandeur, Groupe=Groupe, Qrcode=Qrcode)
