17340cc2ca90943f54f5a82da31ebb369acde545
saliou33 <107521279+saliou33@users.noreply.github.com>
2022-11-08 21:24:57 +0000

    Update Setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-export DATABASE_URL= ' postgres://xkrjzpnssuzcaj:6eec3425638ee63ac7be70453d56abccf3871a8a9f9080abc9a12a07ee46578c@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d2savvsk3pbtjn'
+export DATABASE_URL= 'postgres://xkrjzpnssuzcaj:6eec3425638ee63ac7be70453d56abccf3871a8a9f9080abc9a12a07ee46578c@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d2savvsk3pbtjn'
+
+export WORK_ENV = 'PROD'
